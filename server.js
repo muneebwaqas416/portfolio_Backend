cc58b6517c7c71974a3c047d230660dfd3c7f3df
muneebwaqas416 <muneebwaqas416@gmail.com>
2024-07-28 14:13:56 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config()
+
+// server used to send send emails
+const app = express();
+app.use(cors({
+  origin: 'https://frontend-service.onrender.com', // Replace with your frontend URL
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+app.use(express.json());
+app.use("/", router);
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_USER,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+app.get("/" , (req,res)=>{
+  console.log("in it")
+    res.json({message : "Hello from server"})
+})
+
+
+app.listen(3001, () => console.log("Server Running"));
