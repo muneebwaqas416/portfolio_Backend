16b7c5c6683055bf373ee37e250751cd17800d6f
muneebwaqas416 <muneebwaqas416@gmail.com>
2024-07-28 15:03:35 +0500

    New

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ require("dotenv").config()
 
 // server used to send send emails
 const app = express();
+const corsOptions = {
+  origin: 'https://devportfolio900.onrender.com', // Replace with your actual frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
+  credentials: true,
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
 app.use(express.json());
-app.use(cors({
-  origin: 'https://devportfolio900.onrender.com/', // Replace with your frontend URL
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
 app.use("/", router);
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
